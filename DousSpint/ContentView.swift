d1bff4803b20e217bb05935c3b5674b87a582ffc
Chicha342 <86342851+Chicha342@users.noreply.github.com>
2025-09-29 23:01:57 +0300

    create project

---
+++ DousSpint/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DousSpint
+//
+//  Created by Никита on 29.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Dous Spint")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DousSpint/DousSpintApp.swift
@@ -0,0 +1,17 @@
+//
+//  DousSpintApp.swift
+//  DousSpint
+//
+//  Created by Никита on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct DousSpintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
